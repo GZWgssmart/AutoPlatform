d889bbc819bf57441a664b9a5523b32a2b36e7a7
levct <2303247590@qq.com>
2017-04-25 09:15:12 +0800

    修复少许bug

--- web/js/accessories/accessories_buy.js
+++ web/js/accessories/accessories_buy.js
@@ -4,30 +4,42 @@ $(document).ready(function () {
     initDateTimePicker("form_datetime");
     initTable("cusTable", "pager");
 
-    $("#accSelect").fadeToggle(225);
-
     $("#isAcc").bootstrapSwitch({
         onText: '是',
         offText: '否',
         onColor: 'success',
         offColor: 'danger',
         size: 'normal',
-
         onSwitchChange: function (event, state) {
             if (state == true) {
                 isAcc = true;
-                $("#accSelect").fadeIn(225);
+                showAccessories();
             } else if (state == false) {
                 isAcc = false;
-                $("#accSelect").fadeToggle(225);
             }
         }
     });
+
+    $("#accWin").on("hide.bs.modal", function () {
+        $("#isAcc").bootstrapSwitch("state", false);
+    });
 });
+
+function disableInput() {
+    $("#accName").prop("disabled", true);
+
+}
+
+function onState() {
+    $("#accWin").on("hide.bs.modal", function () {
+        disableInput();
+        $("#isAcc").bootstrapSwitch("state", true);
+    });
+}
 
 /** 编辑数据 */
 function showEditWin() {
-    var selectRow = $("#cusTable").bootstrapTable('getSelections');
+    var selectRow = $("#cusTable").bootstrapTable('ions');
     if (selectRow.length != 1) {
         swal('编辑失败', "只能选择一条数据进行编辑", "error");
         return false;
@@ -41,7 +53,7 @@ function showEditWin() {
 
 /**更新数据 */
 function updateAccessoriesBuyInfo() {
-    $.post(contextPath + "/accessoriesBuy/update",
+    $.post("/accessoriesBuy/update",
         $("#editForm").serialize(),
         function (data) {
             if (data.result == "success") {
@@ -56,7 +68,7 @@ function updateAccessoriesBuyInfo() {
 
 /**添加采购信息 */
 function addAccessoriesBuyInfo() {
-    $.post(contextPath + "/accessoriesBuy/isAccAdd?state=" + isAcc,
+    $.post("/accessoriesBuy/isAccAdd?state=" + isAcc,
         $("#addForm").serialize(),
         function (data) {
             if (data.result == "success") {
@@ -71,48 +83,38 @@ function addAccessoriesBuyInfo() {
 }
 
 
-function operateFormatter(value, row, index) {
-    if (row.accBuyStatus == 'Y') {
-        return [
-            '<button type="button" class="updateActive btn btn-danger  btn-sm" style="margin-right:15px;">冻结</button>',
-            '<button type="button" class="showUpdateIncomingType1 btn btn-primary  btn-sm" style="margin-right:15px;" >编辑</button>'
-        ].join('');
-    } else {
-        return [
-            '<button type="button" class="updateInactive btn btn-success  btn-sm" style="margin-right:15px;" >激活</button>',
-            '<button type="button" class="showUpdateIncomingType1 btn btn-primary  btn-sm" style="margin-right:15px;">编辑</button>'
-        ].join('');
-    }
-
+function fmtOperate(value, row, index) {
+    return [
+        '<button type="button" class="showEditWin btn btn-primary  btn-sm" style="margin-right:15px;" >编辑</button>',
+        '<button type="button" class="removeBuy btn btn-danger  btn-sm" style="margin-right:15px;">删除</button>'
+    ].join('');
 }
+
 window.operateEvents = {
-    'click .updateActive': function (e, value, row, index) {
-        var status = 'N';
-        $.get(contextPath + "/accessoriesBuy/updateStatus?id=" + row.accBuyId + "&status=" + status,
-            function (data) {
+    'click .removeBuy': function (e, value, row, index) {
+        swal({
+            title: "您确定要删除吗？",
+            text: "",
+            type: "warning",
+            showCancelButton: true,
+            closeOnConfirm: false,
+            cancelButtonText: "取消",
+            confirmButtonText: "是的，我要删除",
+            confirmButtonColor: "#ec6c62"
+        }, function () {
+            $.get("/accessoriesBuy/remove?id=" + row.accBuyId + "&status=" + row.accBuyCheck, function (data) {
                 if (data.result == "success") {
                     $('#addWin').modal('hide');
-                    // swal(data.message, "", "success");
+                    swal(data.message, "", "success");
                     $('#cusTable').bootstrapTable('refresh');
                 } else if (data.result == "fail") {
-                    // swal(data.message, "", "error");
+                    swal(data.message, "", "error");
                 }
-            }, "json");
+            });
+        });
+
     },
-    'click .updateInactive': function (e, value, row, index) {
-        var status = 'Y';
-        $.get(contextPath + "/accessoriesBuy/updateStatus?id=" + row.accBuyId + "&status=" + status,
-            function (data) {
-                if (data.result == "success") {
-                    $('#addWin').modal('hide');
-                    // swal(data.message, "", "success");
-                    $('#cusTable').bootstrapTable('refresh');
-                } else if (data.result == "fail") {
-                    // swal(data.message, "", "error");
-                }
-            }, "json");
-    },
-    'click .showUpdateIncomingType1': function (e, value, row, index) {
+    'click .showEditWin': function (e, value, row, index) {
         var accessoriesBuy = row;
         $("#editForm").fill(accessoriesBuy);
         $("#buyTime").val(formatterDate(accessoriesBuy.accBuyTime));
@@ -131,9 +133,51 @@ function addAccBuy() {
     } else {
         var acc = selectRow[0];
         $("#addForm").fill(acc);
+        onState();
         $("#accWin").modal("hide");
     }
-    // $("#accUnit").val(sr.accUnit);
-    // $("#accBuyCount").val(sr.accBuyCount);
+}
+
+function fmtCheckState(value) {
+    if (value == 'Y') {
+        return "已审核";
+    } else {
+        return "审核中";
+    }
+}
+
+function fmtBuyState(value) {
+    if (value == 'Y') {
+        return "已采购";
+    } else {
+        return "未采购";
+    }
+}
+
+function delteleBuy() {
+    var selectRows = $("#cusTable").bootstrapTable('getSelections');
+    var accBuyIdArr = [];
+    for (var i = 0; i < selectRows.length; i++) {
+        accBuyIdArr[i] = selectRows[i].accBuyId;
+    }
+    if (selectRows.length > 0) {
+        $.each(selectRows, function (index, value, array) {
+            if (value.accBuyCheck == "Y") {
+                $.get("/accessoriesBuy/batchDelete?accBuyArr=" + accBuyIdArr, function (data) {
+                    if (data.result == "success") {
+                        swal(data.result, "", "success");
+                        $('#cusTable').bootstrapTable('refresh');
+                    } else if (data.result == "fail") {
+                        swal(data.result, "", "error");
+                    }
+                });
+            } else if (value.accBuyCheck == "N") {
+                swal('删除失败', "必须选择审核通过的数据", "error");
+            }
+        });
+
+    } else {
+        swal('删除失败', "请至少选择一条数据删除", "error");
+    }
 }
 
