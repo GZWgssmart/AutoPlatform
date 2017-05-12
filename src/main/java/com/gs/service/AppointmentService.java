b9653cafe54ee3e453b6fd595c3f2cc433a71216
GZWcb <751545577@qq.com>
2017-05-12 08:46:01 +0800

    提交AppointmentServer

--- src/main/java/com/gs/service/AppointmentService.java
+++ src/main/java/com/gs/service/AppointmentService.java
@@ -1,6 +1,7 @@
 package com.gs.service;
 
 import com.gs.bean.Appointment;
+import com.gs.bean.User;
 import com.gs.common.bean.Pager;
 import org.apache.ibatis.annotations.Param;
 
@@ -8,16 +9,15 @@ import java.util.List;
 
 public interface AppointmentService extends BaseService<String, Appointment>{
 
+    public int countByStatus(String status, User user);
 
-    public int countByStatus(String status);
+    public List<Appointment> queryPagerByStatus(Pager pager, String status,User user);
 
-    public List<Appointment> queryPagerByStatus(Pager pager, String status);
+    public List<Appointment> querySpeedStatus(Pager pager,User user);
 
-    public List<Appointment> querySpeedStatus(Pager pager);
+    public int countByCondition(Appointment appointment,User user);
 
-    public int countByCondition(Appointment appointment);
-
-    public List<Appointment> queryPagerByCondition(Pager pager, Appointment appointment);
+    public List<Appointment> queryPagerByCondition(Pager pager, Appointment appointment,User user);
 
     public int updateSpeedStatusById(String speedStatus, String id);
 
