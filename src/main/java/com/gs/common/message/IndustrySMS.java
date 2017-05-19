e873f2f2030e336f6457747bcbf5a9d1d4129e14
Wjhsmart <672630243@qq.com>
2017-05-19 19:49:10 +0800

    修复发送短信的类

--- src/main/java/com/gs/common/message/IndustrySMS.java
+++ src/main/java/com/gs/common/message/IndustrySMS.java
@@ -12,16 +12,21 @@ import com.gs.common.message.HttpUtil;
  */
 public class IndustrySMS
 {
-	private static String operation = "/industrySMS/sendSMS";
+	private String operation = "/industrySMS/sendSMS";
 
-	private static String accountSid = Config.ACCOUNT_SID;
-	private static String to = "1361305****";
-	private static String smsContent = "【秒嘀科技】您的验证码是345678，30分钟输入有效。";
+	private String accountSid = Config.ACCOUNT_SID;
+	private String to;
+	private String smsContent;
+
+	public IndustrySMS(String to, String smsContent) {
+		this.to = to;
+		this.smsContent = smsContent;
+	}
 
 	/**
 	 * 验证码通知短信
 	 */
-	public static void execute()
+	public void execute()
 	{
 		String url = Config.BASE_URL + operation;
 		String body = "accountSid=" + accountSid + "&to=" + to + "&smsContent=" + smsContent
